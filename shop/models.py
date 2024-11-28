a3aeb09c9134c09f8ce9483435328bcadbf054d7
CezarHoraDev <92612712+CezarinoHora@users.noreply.github.com>
2024-11-27 21:38:47 -0400

    novembro

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['nome']
+        indexes = [
+            models.Index(fields=['nome']),
+        ]
+        verbose_name='categoria'
+        verbose_name_plural='categorias'
